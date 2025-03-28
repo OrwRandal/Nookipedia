9c69503fd9db7ebbaacca72c911a1978cb91d6c8
KristenSantos <Kristensxntos@gmail.com>
2025-03-28 13:51:33 -0400

    started

---
+++ AnimalCrossing/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
